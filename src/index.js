7c9188d57853131710e40c9c136a8dbb2e152de5
Yasin Odabas <ysnodabas@gmail.com>
2019-08-23 00:01:06 +0800

    new component folder

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import MyInfo from "./Components/MyInfo.jsx";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
+//
+// function MyApp() {
+//   return (
+//     <ul>
+//       <li>first</li>
+//       <li>second</li>
+//       <li>third</li>
+//     </ul>
+//   );
+// }
+//
+// ReactDOM.render(<MyApp />, document.getElementById("root"));
+//
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
